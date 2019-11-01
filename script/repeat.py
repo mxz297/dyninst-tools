21279eaeef9eae3543dbe8e327b65dfe7775cc23
Xiaozhu Meng <mxz297@gmail.com>
2019-10-31 20:28:48 -0500

    Update repeat script

--- script/repeat.py
+++ script/repeat.py
@@ -9,19 +9,27 @@ def getParameters():
      parser.add_argument("--exe", type=str, default="./micro-parse-func-level", help="the path to the microbenchmark executeable")
      parser.add_argument("--rep", type=int, default=100, help="the number of times of execution for calculating average run time")
      parser.add_argument("--thread", type=int, default=16, help="the number of threads")
+     parser.add_argument("--cmp", type=int, default=1, help="Whether or not to compare results from single threaded run to multi-threaded runs")
+     parser.add_argument("--valgrind", type=int, default=0, help="Whether or not run with valgrind for memory check")
      args = parser.parse_args()
      return args
 
-def Run(filepath, thread):
-    cmd = "OMP_NUM_THREADS={1} {0} {2} > t{1}.txt".format(args.exe, thread, filepath)
+def Run(filepath, thread, i):
+    if args.valgrind > 0:
+        cmd = "OMP_NUM_THREADS={1} valgrind {0} {2} > t{3}.txt 2>&1".format(args.exe, thread, filepath, i)
+    else:
+        cmd = "OMP_NUM_THREADS={1} {0} {2} > t{1}.txt".format(args.exe, thread, filepath)
     p = Popen(cmd, stdout=PIPE, stderr=PIPE,  shell=True)
     msg, err = p.communicate()
-    if thread > 1:
+    if thread > 1 and args.cmp > 0:
         cmd = "diff t1.txt t{0}.txt".format(thread)
         p = Popen(cmd, stdout=PIPE, stderr=PIPE,  shell=True)
         msg, err = p.communicate()
         if len(msg) > 0:
             print "There are difference"
+            sys.exit()
+        if len(err) > 0:
+            print err
             sys.exit()
 
 args = getParameters()
@@ -34,6 +42,8 @@ else:
 
 for filepath in bins:
     print "Experiment for", filepath
-    Run(filepath, 1)
+    if args.cmp > 0:
+        Run(filepath, 1, 0)
     for i in range(args.rep):
-        Run(filepath, args.thread)
+        print i
+        Run(filepath, args.thread, i)
